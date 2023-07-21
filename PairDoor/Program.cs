3a8ac03f8a4242c1a1437d48f32bef9eb51f377b
梁健 <liang.jian@synyi.com>
2023-07-21 11:26:04 +0800

    自动门和金属资源LCD显示

---
+++ PairDoor/Program.cs
@@ -0,0 +1,114 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        private Dictionary<string, PairDoor> _pairDoors = new Dictionary<string, PairDoor>();
+        private Dictionary<IMyDoor, string> _mustCloseDoor = new Dictionary<IMyDoor, string>();
+
+        public Program()
+        {
+            InitPairDoor();
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            foreach (var keyvalue in _pairDoors)
+            {
+                var pairDoor = keyvalue.Value;
+                if (pairDoor.ExceptStatus)
+                {
+                    pairDoor.Door1.CloseDoor();
+                    pairDoor.Door2.CloseDoor();
+                    _mustCloseDoor[pairDoor.ExceptDoor] = keyvalue.Key;
+                }
+            }
+
+            foreach (var key in _mustCloseDoor.Keys)
+            {
+                if (key.Status == DoorStatus.Closed)
+                {
+                    var pairDoor = _pairDoors[_mustCloseDoor[key]];
+                    if (pairDoor.Door1 == key)
+                        pairDoor.Door2.OpenDoor();
+                    else
+                        pairDoor.Door1.OpenDoor();
+                    _mustCloseDoor.Remove(key);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 初始化门的配对关系
+        /// </summary>
+        private void InitPairDoor()
+        {
+            List<IMyDoor> allDoors = new List<IMyDoor>();
+            GridTerminalSystem.GetBlocksOfType(allDoors);
+
+            foreach (var door in allDoors)
+            {
+                var name = door.DisplayNameText.ToUpper();
+                if (name.Contains("PAIR"))
+                {
+                    var groupName = name.Substring(name.LastIndexOf("PAIR"));
+                    if (!_pairDoors.ContainsKey(groupName))
+                        _pairDoors[groupName] = new PairDoor() { Door1 = door };
+                    else
+                    {
+                        if (_pairDoors[groupName].Door1 != null)
+                            _pairDoors[groupName].Door2 = door;
+                        else
+                            _pairDoors[groupName].Door1 = door;
+                    }
+                }
+            }
+        }
+
+        class PairDoor
+        {
+            public IMyDoor Door1 { get; set; }
+            public IMyDoor Door2 { get; set; }
+
+            public IMyDoor ExceptDoor { get; private set; }
+
+            public bool ExceptStatus
+            {
+                get
+                {
+                    if (Door1.Status == DoorStatus.Opening && Door2.Status != DoorStatus.Closed)
+                    {
+                        ExceptDoor = Door2;
+                        return true;
+                    }
+                    else if (Door2.Status == DoorStatus.Opening && Door1.Status != DoorStatus.Closed)
+                    {
+                        ExceptDoor = Door1;
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ ResourceLCD/Program.cs
@@ -0,0 +1,203 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+using VRageRender;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        private List<IMyInventory> _allInventories = new List<IMyInventory>();
+
+        private Dictionary<string, string> _translate = new Dictionary<string, string>();
+
+        private int _delayTick = 0;
+        private const int DELAY_TIME = 5;
+
+        private Vector2 _vectorTmp = new Vector2(0, 0);
+        private List<IMyTextSurface> _textSurfaces;
+
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+            List<IMyCargoContainer> allCargos = new List<IMyCargoContainer>();
+            GridTerminalSystem.GetBlocksOfType(allCargos);
+            foreach (var cargo in allCargos)
+            {
+                var inventory = cargo.GetInventory();
+                if (inventory != null)
+                    _allInventories.Add(inventory);
+            }
+
+            List<IMyTextPanel> allTextPanels = new List<IMyTextPanel>();
+            GridTerminalSystem.GetBlocksOfType(allTextPanels);
+            foreach (var textPanel in allTextPanels)
+            {
+                var name = textPanel.DisplayNameText;
+                if (name.Contains("金属材料"))
+                    _textSurfaces.Add(textPanel as IMyTextSurface);
+            }
+
+            _translate["Cobalt"] = "钴";
+            _translate["Gold"] = "金";
+            _translate["Stone"] = "石头";
+            _translate["Iron"] = "铁";
+            _translate["Magnesium"] = "镁";
+            _translate["Nickel"] = "镍";
+            _translate["Scrap"] = "废金属";
+            _translate["Platinum"] = "铂金";
+            _translate["Silicon"] = "硅";
+            _translate["Silver"] = "银";
+            _translate["Uranium"] = "铀";
+            _translate["Ice"] = "冰";
+            _translate["Organic"] = "有机物";
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            if (_delayTick++ > DELAY_TIME)
+                _delayTick = 0;
+            else
+                return;
+
+            SortedDictionary<string, long> ingotsCount = new SortedDictionary<string, long>();
+            SortedDictionary<string, long> oresCount = new SortedDictionary<string, long>();
+
+            this.GetInventoryCount(ingotsCount, oresCount);
+
+            foreach (var surface in _textSurfaces)
+            {
+                var frame = surface.DrawFrame();
+                var splitLines = this.GetSplitLine(surface.SurfaceSize.X, surface.SurfaceSize.Y);
+                frame.AddRange(splitLines);
+
+                float x = 2, y = 2;
+                foreach (var ingot in ingotsCount)
+                {
+                    var translateName = _translate[ingot.Key];
+                    if (translateName == "石头")
+                        translateName = "沙石";
+                    else
+                        translateName += "锭";
+                    translateName += "：" + (ingot.Value / 1000000).ToString() + "千克";
+
+                    frame.Add(this.GetTextSprite(translateName, x, y));
+                    y += 20;
+                }
+
+                x += surface.SurfaceSize.Y / 2;
+                y = 2;
+                foreach (var ore in oresCount)
+                {
+                    var translateName = _translate[ore.Key];
+                    if (translateName != "石头" && translateName != "废金属")
+                        translateName += "矿";
+                    translateName += "：" + (ore.Value / 1000000).ToString() + "千克";
+
+                    frame.Add(this.GetTextSprite(translateName, x, y));
+                    y += 20;
+                }
+
+                frame.Dispose();
+            }
+        }
+
+        private void GetInventoryCount(SortedDictionary<string, long> ingotsCount, SortedDictionary<string, long> oresCount)
+        {
+
+            foreach (var inventory in _allInventories)
+            {
+                List<MyInventoryItem> items = new List<MyInventoryItem>();
+                inventory.GetItems(items);
+                foreach (var item in items)
+                {
+                    var typeId = item.Type.TypeId;
+                    if (typeId == "MyObjectBuilder_Ingot")
+                    {
+                        if (ingotsCount.ContainsKey(item.Type.SubtypeId))
+                            ingotsCount[item.Type.SubtypeId] += item.Amount.RawValue;
+                        else
+                            ingotsCount[item.Type.SubtypeId] = item.Amount.RawValue;
+                    }
+                    else if (typeId == "MyObjectBuilder_Ore")
+                    {
+                        if (oresCount.ContainsKey(item.Type.SubtypeId))
+                            oresCount[item.Type.SubtypeId] += item.Amount.RawValue;
+                        else
+                            oresCount[item.Type.SubtypeId] = item.Amount.RawValue;
+                    }
+                }
+            }
+        }
+
+        private MySprite GetTextSprite(string text, float x, float y)
+        {
+            _vectorTmp.X = x;
+            _vectorTmp.Y = y;
+            return new MySprite()
+            {
+                Type = SpriteType.TEXT,
+                Data = text,
+                Position = _vectorTmp,
+                RotationOrScale = 0.8f,
+                Color = Color.Red,
+                Alignment = TextAlignment.CENTER,
+                FontId = "White"
+            };
+        }
+
+        private List<MySprite> GetSplitLine(float width, float height)
+        {
+            List<MySprite> result = new List<MySprite>();
+
+            var sprite1 = new MySprite()
+            {
+                Type = SpriteType.TEXTURE,
+                Data = "Grid",
+                Color = Color.White.Alpha(0.66f),
+                Alignment = TextAlignment.CENTER
+            };
+            _vectorTmp.X = width / 4;
+            _vectorTmp.Y = height / 2;
+            sprite1.Position = _vectorTmp;
+            _vectorTmp.X = width / 2;
+            _vectorTmp.Y = height;
+            sprite1.Size = _vectorTmp;
+
+            var sprite2 = new MySprite()
+            {
+                Type = SpriteType.TEXTURE,
+                Data = "Grid",
+                Color = Color.White.Alpha(0.66f),
+                Alignment = TextAlignment.CENTER
+            };
+            _vectorTmp.X = width * 3 / 4;
+            _vectorTmp.Y = height / 2;
+            sprite2.Position = _vectorTmp;
+            _vectorTmp.X = width / 2;
+            _vectorTmp.Y = height;
+            sprite2.Size = _vectorTmp;
+
+            result.Add(sprite1);
+            result.Add(sprite2);
+
+            return result;
+        }
+    }
+}
